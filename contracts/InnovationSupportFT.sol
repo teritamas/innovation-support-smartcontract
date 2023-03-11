d5dfb717932706b197711a7ddd930f79d640914f
sey323 <sey323_nm@yahoo.co.jp>
2023-03-11 16:45:32 +0900

    ISFT発行用のコントラクトの作成

--- contracts/InnovationSupportFT.sol
+++ contracts/InnovationSupportFT.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract InnovationSupportFT is ERC20 {
+contract InnovationSupportFT is ERC20, Ownable {
+
+    // デポジットとして入金する額: 1token = 1千円として50,000千円入金する
+    uint256 private constant depositAmmount = 50 * 1000;
 
     constructor() ERC20("InnovationSupportFT", "ISFT"){
-        mint(msg.sender, 1000 * 1000 * 2000);
+        mint(msg.sender, depositAmmount);
     }
 
     /**
@@ -25,7 +28,7 @@ contract InnovationSupportFT is ERC20 {
      * 指定したアドレスにトークンを発行
      */
     function mint(address senderAddress, uint256 amount )
-        public
+        private
     {
         _mint(senderAddress, amount);
         emit TokenMint(senderAddress, amount);
@@ -37,7 +40,27 @@ contract InnovationSupportFT is ERC20 {
      */
     function burn(address senderAddress, uint256 amount )
         public
+        onlyOwner
     {
         _burn(senderAddress, amount);
+    }
+
+    /**
+     * @dev
+     * 指定したアドレスのトークンを焼却
+     */
+    function mintDeposit() 
+        public
+        onlyOwner
+    {
+        mint(msg.sender, depositAmmount);
+    }
+
+    /**
+     * @dev
+     * デポジットされているトークン量を取得
+     */
+    function balanceOfDeposit() public view virtual returns (uint256) {
+        return balanceOf(msg.sender);
     }
 }
