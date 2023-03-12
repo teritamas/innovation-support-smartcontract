b258a0c1a3220b367e2429cec365b5255a99a941
Ichirou.S <sey323@users.noreply.github.com>
2023-03-12 14:47:27 +0900

    投票コントラクトからNFTの発行 (#8)

--- contracts/ProposalNFT.sol
+++ contracts/ProposalNFT.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ProposalNFT is ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 private _tokneIdCounter = 0;
+
+    mapping(uint256 => uint8) private _tokenAmount;
 
     event TokenURIChanged(
         address indexed sender,
@@ -21,19 +21,56 @@ contract ProposalNFT is ERC721URIStorage, Ownable {
     constructor() ERC721("ProposalNFT", "ISPNFT") {}
 
     /**
-     * @dev
-     * 提案NFTを作成
+     * 提案NFTの作成
      */
-    function nftMint(address proposerAddress, string memory tokenUri)
+    function mintNft(address proposerAddress, string memory tokenUri, uint8 tokenAmount)
         public
         onlyOwner
     {
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
+        _tokneIdCounter += 1; 
+        uint256 newTokenId = _tokneIdCounter;
 
+        _mintAndSetUri(proposerAddress, tokenUri, newTokenId, tokenAmount);
+    }
+
+    /**
+     * トークンIDを指定して提案NFTの作成
+     */
+    function mintNftFromTokenId(address proposerAddress, string memory tokenUri, uint256 tokenId, uint8 tokenAmount)
+        public
+        onlyOwner
+    {
+        require(_tokneIdCounter < tokenId, "This tokenId is already used.");
+
+        _tokneIdCounter = tokenId; // カウンターのtokenIdを最新にする
+        uint256 newTokenId = _tokneIdCounter; 
+        
+        _mintAndSetUri(proposerAddress, tokenUri, newTokenId, tokenAmount);
+    }
+
+    /**
+     * 提案により取得したい金額を設定する
+     */ 
+    function setTokenAmount(uint256 newTokenId, uint8 tokenAmount) public {
+        _tokenAmount[newTokenId] = tokenAmount;
+    }
+
+    /**
+     * NFTに紐づく調達金額を取得する
+     */ 
+    function getTokenAmount(uint256 tokenId) public view returns(uint8) {
+        return _tokenAmount[tokenId];
+    }
+
+    /**
+     * NFTを発行しURIを設定する
+     */
+    function _mintAndSetUri(address proposerAddress, string memory tokenUri, uint256 newTokenId, uint8 tokenAmount) private {
         _mint(proposerAddress, newTokenId);
         _setTokenURI(newTokenId, tokenUri);
+        setTokenAmount(newTokenId, tokenAmount);
 
         emit TokenURIChanged(proposerAddress, newTokenId, tokenUri);
     }
+
 }
